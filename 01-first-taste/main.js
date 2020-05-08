4ebda0e9cd261d594cdb0eef6f34fe7375568cfd
ndpniraj <ndpniraj@gmail.com>
2020-05-08 10:27:40 +0530

    first taste of javascript for youtube.

---
+++ 01-first-taste/main.js
@@ -0,0 +1,9 @@
+// variable
+
+let name = 'Manoj';
+let age = 30;
+let country = 'America';
+let height = 6.1;
+let maritalStatus = false;
+
+printBio(name, age, country, height, maritalStatus);
--- 01-first-taste/no-need/bio-app.js
+++ 01-first-taste/no-need/bio-app.js
@@ -1,6 +1,6 @@
 const printBio = (name, age, country, height, status) => {
 	const text = `Hi, my name is <span>${name}</span>, I am <span>${age}</span> years old. I live in <span>${country}</span>. I am <span>${height}</span> feet tall, and I am a <span>${
-		status ? 'HAPPILY MARRIED MAN' : ' HAPPIEST MAN IN THE WORLD'
+		status ? 'HAPPILY MARRIED MAN' : 'HAPPIEST MAN IN THE WORLD'
 	}!!!</span>`;
 
 	const heading = document.createElement('h1');
